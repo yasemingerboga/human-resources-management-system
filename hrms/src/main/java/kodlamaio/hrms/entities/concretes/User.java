2d73b37508a6a3120568ea8397c2c54a24200f6a
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-06-25 20:47:28 +0300

    update

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/User.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.entities.concretes;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	@ApiModelProperty(hidden = true)
+	private int id;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+
+}
