bf51d414045cd49be21314caf7510f4ce1fcde76
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-06-29 14:35:59 +0300

    Create City entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,40 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="cities")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler", "jobAdvertisements" })
+public class City {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	//@ApiModelProperty(hidden = true)
+	private int id;
+	
+	@Column(name="city_name")
+	private String city;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "city" )
+	private List<JobAdvertisement> jobadvertisements;
+}
