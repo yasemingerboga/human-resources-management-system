563285b187cce37811669163e6d5d5f5f3b5feb4
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-14 13:57:21 +0300

    Create JobExperience

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobExperience.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="job_experiences")
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobExperience {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="workplace_name")
+	private String workPlaceName;
+	
+	@Column(name="job_position")
+	private String jobPosition;
+	
+	@Column(name="start_date")
+	private LocalDate startDate;
+	
+	@Column(name="quit_date")
+	private LocalDate quitDate;
+	
+	@ManyToOne
+	@JoinColumn(name="cv_id")
+	private CV cv;
+}
