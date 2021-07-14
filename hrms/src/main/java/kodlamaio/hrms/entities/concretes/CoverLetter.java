14546c4a6a7c5f3c1261abd8d249defb28fc2503
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-14 13:58:59 +0300

    Create CoverLetter

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/CoverLetter.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="cover_letters")
+@AllArgsConstructor
+@NoArgsConstructor
+public class CoverLetter {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="cover_letter")
+	private String coverLetter;
+	
+	@ManyToOne
+	@JoinColumn(name="cv_id")
+	private CV cv;
+}
