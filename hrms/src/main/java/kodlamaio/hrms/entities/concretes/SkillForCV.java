a7c6f4a66806bff59395d610f4ac386befc2adf3
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-14 13:55:48 +0300

    Create SkillForCV

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/SkillForCV.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="foreign_languages")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SkillForCV {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="skill_id")
+	private Skill skill;
+	
+	@ManyToOne
+	@JoinColumn(name="cv_id")
+	private CV cv;
+}
