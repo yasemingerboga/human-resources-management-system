210acd325975d92693e6763c099b0a38dc02a4be
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-15 16:20:41 +0300

    Create ImageManager

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/ImageManager.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import kodlamaio.hrms.adapters.cloudinaryAdapter.CloudinaryAdapter;
+import kodlamaio.hrms.business.abstracts.ImageService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.ImageDao;
+
+@Service
+public class ImageManager implements ImageService {
+	private ImageDao imageDao;
+
+	@Autowired
+	public ImageManager(ImageDao imageDao) {
+		super();
+		this.imageDao = imageDao;
+	}
+
+	@Override
+	public Result add(MultipartFile file) throws IOException {
+		DataResult<Object> result = CloudinaryAdapter.uploadImage(file);
+		return new SuccessResult(result.getData().toString());
+	}
+}
