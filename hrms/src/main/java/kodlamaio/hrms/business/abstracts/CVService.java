57f3ca7ec0a215a8d5bcbd9d726ff3e6ceebd191
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-15 16:19:57 +0300

    Create CVService

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/CVService.java
@@ -0,0 +1,14 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.CV;
+
+public interface CVService {
+	Result add (CV cv);
+	DataResult<List<CV>> getAll();
+	DataResult<CV> getByCandidate_Id(int id);
+
+}
