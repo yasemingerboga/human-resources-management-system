6294d9b852db94619decdef8cc3299e735522f94
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-15 16:21:42 +0300

    Create UniversityService

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/UniversityService.java
@@ -0,0 +1,13 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.University;
+
+public interface UniversityService {
+	DataResult<List<University>> getAll();
+
+	Result add(University university);
+}
